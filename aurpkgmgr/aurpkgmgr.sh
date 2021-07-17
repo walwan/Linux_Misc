763e876041c0964ce737e24bb74f1b6fc971a541
walwan <11062281+walwan@users.noreply.github.com>
2021-07-17 11:40:57 +0800

    Finish aur package clean function

--- aurpkgmgr/aurpkgmgr.sh
+++ aurpkgmgr/aurpkgmgr.sh
@@ -62,13 +62,13 @@ _askYesOrNo(){
             result=0
             ;;
             *)
-            printf "Unexpected input! Please retry!\n"
+            printf "Unexpected input! Please retry!"
             _askYesOrNo
             result=$?
             ;;
         esac
     else
-        result=default
+        result=${default}
     fi
 
     return ${result}
@@ -112,11 +112,38 @@ _cleanPkg(){
 
     for folder in `ls ${AURPkgDir}`; do
         if test -d "${AURPkgDir}/${folder}"; then
-            pkgCount=0
-            for file in `ls -t ${AURPkgDir}/${folder}`; do
-                ((pkgCount++))
+            pkgCount=()
+            SubPkgName=()
 
-                if test ${pkgCount} -gt ${numPkgKeep}; then
+            for file in `ls -t ${AURPkgDir}/${folder}`; do
+                # Check valid package
+                ChkOutput=`echo ${file} | grep ".pkg.tar"`
+                if test -z ${ChkOutput}; then
+                    continue
+                fi
+
+                # Parsing package name
+                PkgName=`LC_ALL=en pacman -Qip ${AURPkgDir}/${folder}/${file} | grep "Name" | sed "s/^Name[[:space:]]*:[[:space:]]//g"`
+                SubPkgIndex=0
+                if test ${#SubPkgName[@]} -eq 0; then
+                    SubPkgName[${SubPkgIndex}]=${PkgName}
+                    pkgCount[${SubPkgIndex}]=1
+                else
+                    for name in ${SubPkgName[${SubPkgIndex}]}; do
+                        if test ${name} = ${PkgName}; then
+                            ((pkgCount[${SubPkgIndex}]++))
+                            break
+                        fi
+                        ((SubPkgIndex++))
+                    done
+                    if test $SubPkgIndex -eq ${#SubPkgName[@]}; then
+                        SubPkgName[${SubPkgIndex}]=${PkgName}
+                        pkgCount[${SubPkgIndex}]=1
+                    fi
+                fi
+
+                # Check package count
+                if test ${pkgCount[${SubPkgIndex}]} -gt ${numPkgKeep}; then
                     pkgCleanPath[${numPkgClean}]="${AURPkgDir}/${folder}/${file}"
                     ((numPkgClean++))
                 fi
@@ -127,9 +154,20 @@ _cleanPkg(){
     # Output results
     printf "%d package(s) to be cleaned:\n" ${numPkgClean}
     if test ${numPkgClean} -gt 0; then
+        OutputCnt=1
         for file in ${pkgCleanPath[@]}; do
-            printf "%s\n" ${file}
+            printf "%d: %s\n" ${OutputCnt} ${file}
+            ((OutputCnt++))
         done
+
+        printf "Start Cleaning?"
+        _askYesOrNo
+        if test $? -eq 1; then
+            for file in ${pkgCleanPath[@]}; do
+                rm -r ${file}
+            done
+            printf "Done Cleaning!\n"
+        fi
     fi
 }
 
